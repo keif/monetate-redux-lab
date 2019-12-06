2a9592a50adfd23a78dc5035669d89e98833fd23
Stepan <stepan.kolomiiets@dresslerconsulting.com>
2019-12-06 18:47:50 +0200

    Load builder
    This commit invokes load builder function which loads SPA Action Builder
    To monetate-root div element.

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import monetateStateStoreIntegrationSDK from "monetate-state-store-integration-sdk";
 
 import store from './services/store';
+
+monetateStateStoreIntegrationSDK.monetateLoadBuilder(store().getState(), () => {}, "#monetate-root", "#root");
 
 const Root = ({ children, initialState = {} }) => (
   <Provider store={store(initialState)}>{children}</Provider>
